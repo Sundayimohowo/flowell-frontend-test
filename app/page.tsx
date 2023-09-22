7e89233d1ae90c60dddba306a1d31f53dcb90c37
Sundayimohowo <sundayimohowo@gmail.com>
2023-09-22 06:20:21 +0100

    refactor: quick refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 import { fetchProductsApi } from "@/api/products.api";
 import { IPagination } from "@/interfaces/page.info";
 import { ProductType } from "@/interfaces/products";
-import { useState, useEffect } from "react";
 
 export default function Home() {
   const [uniqueProducts] = useState<Record<string, ProductType>>({});
@@ -19,21 +20,23 @@ export default function Home() {
 
   useEffect(() => {
     fetchProductsApi(pageOptions)
-      .then((res) => {
-        // prevent product duplications
-        const _products = res.data.data.filter((product) => {
-          if (!uniqueProducts[product._id]) {
-            uniqueProducts[product._id] = product;
-            return product._id;
-          } else return false;
-        });
-        const _pageInfo = res.data.pageInfo;
+      .then((res: { data: IPagination<ProductType>; status?: number }) => {
+        if (res?.status === 200) {
+          // prevent product duplications
+          const _products = res.data.data.filter((product) => {
+            if (!uniqueProducts[product._id]) {
+              uniqueProducts[product._id] = product;
+              return product._id;
+            } else return false;
+          });
+          const _pageInfo = res.data.pageInfo;
 
-        // update product list
-        setProducts((prev) => ({
-          data: [...prev?.data, ..._products],
-          pageInfo: { ...prev?.pageInfo, ..._pageInfo },
-        }));
+          // update product list
+          setProducts((prev) => ({
+            data: [...prev?.data, ..._products],
+            pageInfo: { ...prev?.pageInfo, ..._pageInfo },
+          }));
+        }
       })
       .catch((error) => {
         console.log("🚀 ~ file: page.tsx:12 ~ .then ~ res:", error);
